a69ea16423f6763aef7747eee7c55897e3570c8b
rychly-lukas <rychly-lukas@seznam.cz>
2021-03-11 11:20:10 +0100

    04 - úprava dle PDF

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Genre name",
+                            help_text='Enter a film genre (e.g. sci-fi, comedy)')
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        """ Řetězec, který se používá jako textová reprezentace objektu (například v
+       administraci aplikace).
+        V našem případě bude objekt (žánr) reprezentován výpisem obsahu pole name """
+        return self.name
