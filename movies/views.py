c6cc1f85803493906dc9d38d76e0567b7d3006a4
rychly-lukas <rychly-lukas@seznam.cz>
2021-03-25 11:13:19 +0100

    09 - online hodina - 25. 3. 2021

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from movies.models import *
 
-# Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    """Metoda připravuje pohled pro domovskou stránku - šablona index.html"""
+
+    # Uložení celkového počtu filmů v databázi do proměnné num_films
+    num_films = Film.objects.all().count()
+    # Do proměnné films se uloží 3 filmy uspořádané podle hodnocení (sestupně)
+    films = Film.objects.order_by('-rate')[:3]
+
+    """ Do proměnné context, která je typu slovník (dictionary) uložíme hodnoty obou proměnných """
+    context = {
+        'num_films': num_films,
+        'films': films
+    }
+
+    """ Pomocí metody render vyrendrujeme šablonu index.html a předáme ji hodnoty v proměnné context k zobrazení """
+    return render(request, 'index.html', context=context)
+
 
 def topten(request):
     return render(request, 'topten.html')
+
